60b7d73a180658b912c3c411a3511a1baac66dbf
raihanshaikhh <raihanshaikh91201@gmail.com>
2026-08-28 16:00:06 +0530

    added profile pic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import profile from "../assets/profile.jpg";
+import profile from "../assets/profile.png";
 
 const fadeUp = {
   hidden: {
